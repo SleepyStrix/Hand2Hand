5e871eb92fe54caa61fce2663ff4e14d3bb499ca
Daniel Hewlett <DanCHewlett@gmail.com>
2015-10-17 02:59:09 -0400

    Initial Commit

---
+++ Assets/PlayerHealthHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealthHandler : MonoBehaviour {
+
+	public int leftHP = 100; //player 1 health (left hand)
+	public int rightHP = 100; //player 2 health (right hand)
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void damage(string hand) {
+		if (hand.Equals("Left")) {
+			leftHP--;
+		}
+		else {
+			rightHP--;
+		}
+	}
+}
