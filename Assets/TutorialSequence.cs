61ae938fad22a498d61403e099cc823494e17252
Daniel Hewlett <DanCHewlett@gmail.com>
2015-10-17 22:39:45 -0400

    Music added and basic instructions

---
+++ Assets/TutorialSequence.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialSequence : MonoBehaviour {
+
+	public GameObject[] seq;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("tutSeq");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public IEnumerator tutSeq() {
+		seq [0].active = true;
+		yield return new WaitForSeconds(5);
+		seq [1].active = true;
+		yield return new WaitForSeconds(5);
+		seq [2].active = true;
+		yield return new WaitForSeconds(5);
+		seq [3].active = true;
+		yield return new WaitForSeconds(5);
+		seq [0].active = false;
+		seq [1].active = false;
+		seq [2].active = false;
+		seq [3].active = false;
+		seq [4].active = true;
+		yield return new WaitForSeconds(2);
+		Destroy (gameObject);
+	}
+}
